934bce62a0e96c23829c737f2c3f6ffa596eb31f
agadiffe <agadiffe@student.42.fr>
2017-11-28 20:05:50 +0100

    add reallocf

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -323,3 +323,13 @@ void	*calloc(size_t nmemb, size_t size)
 	}
 	return (NULL);
 }
+
+void	*reallocf(void *ptr, size_t size)
+{
+	void	*new;
+
+	new = realloc(ptr, size);
+	if (!new)
+		free(ptr);
+	return (new);
+}
